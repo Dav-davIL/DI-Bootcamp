f583b0fe959831cdbf5a85eaca85533f3dead8a9
David Illouz <illouz.dav@gmail.com>
2021-12-04 23:45:42 +0200

    week5 day2

--- Week7/Day4/ExerciseNinja/Exercise2.py
+++ Week7/Day4/ExerciseNinja/Exercise2.py
@@ -1,59 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 17 19:40:14 2021
+Spyder Editor
 
-@author: lenovo
+This is a temporary script file.
 """
 
-class Farm:
-    
-    def __init__(self,name):
-        self.name = name
-        self.animals = {}
-        
-        
-    def add_animal(self,type_of_animal,number = 1):
-        if type_of_animal in self.animals.keys():
-            counter = self.animals[type_of_animal] + number
-            self.animals.update({type_of_animal : counter})
-        else:
-            self.animals.update({type_of_animal : number})
-        
-   
-    def get_info(self):
-        print(f'{self.name}\'s farm')
-        for key,value in self.animals.items():
-            print(key,":", value)
-        return '\n\tE-I-E-I-0!'
-    
-    def get_animal_types(self):
-        list_animal = list(self.animals.keys())
-        for i in range(len(list_animal)):
-            for j in range(i+1, len(list_animal)):
-                if list_animal[i] > list_animal[j]:
-                    temp = list_animal[i]
-                    list_animal[i] = list_animal[j]
-                    list_animal[j] = temp
-        return list_animal
-    
-    def get_short_info(self):
-        my_list = Farm.get_animal_types(self)
-        text = self.name + 's farm has '
-        for i in range(len(my_list)):
-            if i == len(my_list)-1:
-                text += 'and ' + my_list[i]
-            else:
-                text += my_list[i]+'s' + ' '
-        return text
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
 
-        
-macdonald = Farm("McDonald")
-macdonald.add_animal('cow',5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
-macdonald.add_animal('goat', 12)
-print(macdonald.get_animal_types())
-print(macdonald.get_short_info())
-print(macdonald.get_info())
+def encrypt(text):
+    if text != '':
+        text = text.upper()
+        text_encrypted = ''
+        my_list = text.split(" ")
+        for word in range(len(my_list)):
+            for letter in range(len(my_list[word])):
+                text_encrypted += MORSE_CODE_DICT[my_list[word][letter]] + ' '
+            text_encrypted += ' / '
+        return text_encrypted
+    
+textEncrypted = encrypt("david illouz")
 
+def decrypt(text_morse):
+    if text_morse != '':
+        list_morse = text_morse.split(" / ")
+        for word in range(len(list_morse)):
+            for letter in range(len(list_morse[word])):
+                print(list_morse[word][letter])
 
+decrypt('-.. .- ...- .. -..  / .. .-.. .-.. --- ..- --..  /')
